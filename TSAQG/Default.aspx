e1de4fb1ed0018bbd3f03cce4b30cac45f654928
bandarAlrooqi <officialalrooqi@gmail.com>
2022-05-25 00:25:42 +0300

    Add project files.

---
+++ TSAQG/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TSAQG._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="container">
+        <br />
+        <br />
+        <div class="form">
+            <h2 class="fw-light text-center">Text Summarization & Question Generation </h2>
+            <hr id="hr" runat="server" />
+            <div class="row">
+
+                <div class="col col-lg-6 form-group">
+                    <asp:TextBox ID="orignalT" class="form-control form-group-lg w-100" runat="server" TextMode="MultiLine" Style="min-width: 100%; resize: none;" placeholder="Orignal Text" Rows="20"></asp:TextBox>
+
+                </div>
+
+                <div class="col col-lg-6">
+                    <asp:TextBox ID="summaryT" class="form-control form-group-lg" runat="server" Style="min-width: 100%; resize: none; float: right;" placeholder="Summarized Text" Rows="20" TextMode="MultiLine"></asp:TextBox>
+                </div>
+
+            </div>
+            <br />
+            <div class="d-flex justify-content-center">
+                <asp:Button ID="processB" runat="server" CssClass="btn-group-lg btn-secondary form-control" Width="30%" Text="Process" OnClick="processB_Click" />
+
+            </div>
+            <div id="questions" runat="server">
+                <br />
+            </div>
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ TSAQG/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Web.UI;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+
+namespace TSAQG
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void processB_Click(object sender, EventArgs e)
+        {
+            questions.InnerHtml = "";
+             if(orignalT.Text.Split(' ').Length < 200)
+            {
+                hr.InnerHtml = "<div class='alert alert-danger' role='alert'>Orignal text must be 200 token or more.</div>";
+            }
+            else
+            {
+                try
+                {
+                    HttpClient client = new HttpClient();
+                    client.Timeout = TimeSpan.FromHours(1);
+                    client.BaseAddress = new Uri("https://tsaqg-dvtu5xlzta-el.a.run.app/");
+                    HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, "run");
+                    requestMessage.Content = JsonContent.Create(new { text = orignalT.Text });
+                    var sent = client.SendAsync(requestMessage);
+
+                    if (sent.Result.IsSuccessStatusCode)
+                    {
+                        var result = sent.Result.Content.ReadAsAsync<Data>();
+                        var list = result.Result.result;
+
+                        for (int i = 0; i < list.Count - 1; i += 2)
+                        {
+                            questions.InnerHtml +=
+
+                                "<div><div class='input-group mb-3'>" +
+                                    "<span class='input-group-text' id='" + i + "'>❔</span>" +
+                                    "<input type='text' class='form-control' aria-describedby='" + i + "' value='" + list[i] + "' disabled>" +
+                                "</div>";
+                            questions.InnerHtml +=
+                                    "<div class='input-group mb-3'>" +
+                                          "<span class='input-group-text' id='" + (i + 1) + "'>❕</span>" +
+                                          "<input type='text' class='form-control' aria-describedby='" + (i + 1) + "' value='" + list[i + 1] + "' disabled>" +
+                                    "</div></div>";
+                        }
+                        summaryT.Text = list[list.Count - 1];
+                    }
+                    else
+                    {
+                        hr.InnerHtml = "<div class='alert alert-danger' role='alert'>Sorry something went wrong from our side.</div>";
+                    }
+                }catch(Exception err)
+                {
+                    hr.InnerHtml = "<div class='alert alert-danger' role='alert'>Sorry something went wrong from our side.</div>";
+                }
+            }
+        }
+    }
+    struct Data
+    {
+        public List<string> result { get; set; }
+    }
+}
